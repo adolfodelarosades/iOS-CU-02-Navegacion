aa458123bf8f527ed12210d43534e2fa0876f295
Adolfo de la Rosa Peña <adolfodelarosa2012@gmail.com>
2019-08-29 21:23:20 +0200

    Navegación entre pantallas (ViewControllers) usando Segue.

---
+++ iOS-CU-02-Navegacion/SegundoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SegundoViewController.swift
+//  iOS-CU-02-Navegacion
+//
+//  Created by Adolfo De la Rosa on 29/08/2019.
+//  Copyright © 2019 ADP. All rights reserved.
+//
+
+import UIKit
+
+class SegundoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
