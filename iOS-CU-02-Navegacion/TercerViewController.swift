7fd2528b9378c49db243fe6668c31d662f5d2a05
Adolfo de la Rosa Peña <adolfodelarosa2012@gmail.com>
2019-08-30 11:48:24 +0200

    Navegar a una tercer pantalla y regresar de ella a la pantalla anterior.

---
+++ iOS-CU-02-Navegacion/TercerViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TercerViewController.swift
+//  iOS-CU-02-Navegacion
+//
+//  Created by Adolfo De la Rosa on 30/08/2019.
+//  Copyright © 2019 ADP. All rights reserved.
+//
+
+import UIKit
+
+class TercerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func Regresar(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
